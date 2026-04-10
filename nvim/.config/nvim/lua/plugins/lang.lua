cd3e8d5a749512eed530c3a0ff003dc82fdbab06
Pourya <pourya7@gmail.com>
2026-04-10 12:20:12 +0100

    Disable Markdown linting and warnings
    Disabling markdown linting and warnings

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -15,10 +15,19 @@ return {
         html            = { "prettier" },
         json            = { "prettier" },
         yaml            = { "prettier" },
-        markdown        = { "prettier" },
+        -- markdown        = { "prettier" },
       },
     },
   },
+
+	{
+		"mfussenegger/nvim-lint",
+		optional = true,
+		opts = function(_, opts)
+			opts.linters_by_ft = opts.linters_by_ft or {}
+			opts.linters_by_ft.markdown = {}
+		end,
+	},
 
   -- Python: prefer ruff over pyright for formatting/linting speed
   {
